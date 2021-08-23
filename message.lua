fb03693963226bfef8b662c87ce182d8dd080fa7
GenaroYT <81038333+GenaroYT@users.noreply.github.com>
2021-08-23 20:13:12 -0300

    Update message.lua

--- message.lua
+++ message.lua
@@ -1,1 +1,1 @@
-Dialog.Message("Servers down in minutes", "The servers are going to be down in a couple of minutes, so upload and download files as fast as you can...", MB_OK, MB_ICONINFORMATION, MB_DEFBUTTON1);
+Dialog.Message("Servers down FOREVER", "The servers are going to be down forever in a couple of minutes, so upload and download files as fast as you can... Bye Bye :D", MB_OK, MB_ICONINFORMATION, MB_DEFBUTTON1);
